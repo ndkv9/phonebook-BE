0e5f0c1cf0d68512096f2d3e56b50473d3ad0bb8
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-10 12:55:19 +0200

    add unique validator

--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 
@@ -15,9 +16,18 @@ mongoose
 	.catch(err => console.log('error connecting to MongoDB:', err.message))
 
 const personSchema = new mongoose.Schema({
-	name: String,
-	number: String,
+	name: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	number: {
+		type: String,
+		required: true,
+	},
 })
+
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
 	transform: (doc, returnedObj) => {
